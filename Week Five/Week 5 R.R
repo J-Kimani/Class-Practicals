9a1750e30466adce3f0b80fde046ded5d5e1dbe7
J-Kimani <joshuakimani192@gmail.com>
2025-10-22 21:58:01 +0300

    r practicals

--- Week Five/Week 5 R.R
+++ Week Five/Week 5 R.R
@@ -11,3 +11,6 @@ data(mtcars)
 model <- lm(mpg ~ hp + wt, data = mtcars)
 summary(model)
 
+# Example 3: Visualizing Model Fit
+plot(mtcars$hp, mtcars$mpg, main="Regression Fit", xlab="Horsepower", ylab="MPG")
+abline(lm(mpg ~ hp, data=mtcars), col="red")
